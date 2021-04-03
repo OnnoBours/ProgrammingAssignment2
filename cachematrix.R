3797dd6fd6e206c36beaa06f5ffcee09fba97cd3
OnnoBours <onnobours@gmail.com>
2021-04-03 12:07:57 +0200

    done

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Following are two functions to 
 
-## Write a short comment describing this function
+## This function is based on the function makeVector by rdpeng. 
+## m has been exchanged by i and mean by solve.
+## This function may be used to compute the inverse of a matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(solve) i <<- solve
+  getInverse <- function() i
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
+## This function is based on the function cachemean by rdpeng. 
+## m has been exchanged by i and mean by solve.
+## This function may be used to cache the inverse of a matrix.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  i <- x$getmean()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setInverse(i)
+  i
 }
+
